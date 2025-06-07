ba56a81c187c477999ebe2498b575ac87f91f026
Vojtěch Kudláček <vojtech.kudlacek@carvago.com>
2025-06-07 22:34:04 +0200

    rafactor: implement lazy loading

--- website/App.tsx
+++ website/App.tsx
@@ -1,45 +1,19 @@
 import { Flex } from 'antd';
 import { socket } from 'api/socket';
-import {
-	BarElement,
-	CategoryScale,
-	Chart as ChartJS,
-	Filler,
-	Legend,
-	LineController,
-	LineElement,
-	LinearScale,
-	PointElement,
-	Title,
-	Tooltip,
-} from 'chart.js';
-import annotationPlugin from 'chartjs-plugin-annotation';
 import { useSetAtom } from 'jotai';
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { fetchConfigAtom } from 'store/config';
 import { fetchLogsAtom } from 'store/logs';
 import { fetchRecordsAtom } from 'store/records';
-import { AdminZone } from 'ui/containers/AdminZone';
-import { Authentication } from 'ui/containers/Authentication';
-import { ConfigCard } from 'ui/containers/ConfigCard';
 import { CurrentStateCard } from 'ui/containers/CurrentStateCard';
 import { GraphsCard } from 'ui/containers/GraphsCard';
-import { LogsCard } from 'ui/containers/LogsCard';
 
-// Register ChartJS components
-ChartJS.register(
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	LineController,
-	BarElement,
-	Title,
-	Tooltip,
-	Legend,
-	Filler,
-	annotationPlugin,
+const AdminZone = lazy(() => import('ui/containers/AdminZone').then((module) => ({ default: module.AdminZone })));
+const Authentication = lazy(() =>
+	import('ui/containers/Authentication').then((module) => ({ default: module.Authentication })),
 );
+const ConfigCard = lazy(() => import('ui/containers/ConfigCard').then((module) => ({ default: module.ConfigCard })));
+const LogsCard = lazy(() => import('ui/containers/LogsCard').then((module) => ({ default: module.LogsCard })));
 
 export function App() {
 	const fetchRecords = useSetAtom(fetchRecordsAtom);
--- website/main.tsx
+++ website/main.tsx
@@ -1,9 +1,39 @@
 import '@ant-design/v5-patch-for-react-19';
 import './index.css';
 
+import {
+	BarElement,
+	CategoryScale,
+	Chart as ChartJS,
+	Filler,
+	Legend,
+	LineController,
+	LineElement,
+	LinearScale,
+	PointElement,
+	Title,
+	Tooltip,
+} from 'chart.js';
+import annotationPlugin from 'chartjs-plugin-annotation';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
+
+// Register ChartJS components
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	LineController,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+	Filler,
+	annotationPlugin,
+);
 
 const root = document.getElementById('root');
 if (!root) {
