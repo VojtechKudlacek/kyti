89a82f3261e19b082d36ed102e7b85fbd43e37dd
Vojtěch Kudláček <vojtech.kudlacek@carvago.com>
2025-07-01 21:38:49 +0200

    refactor: improve login ux

--- website/ui/containers/Authentication.tsx
+++ website/ui/containers/Authentication.tsx
@@ -1,23 +1,34 @@
-import { Input, Modal } from 'antd';
+import { Form, Input, type InputRef, Modal } from 'antd';
 import { useAtom } from 'jotai';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 import { useState } from 'react';
 import { secretAtom } from 'store/secret';
 import { AuthButton } from 'ui/primitives/AuthButton';
 
+interface FormValues {
+	SECRET: string;
+}
+
 export function Authentication() {
-	const [inputValue, setInputValue] = useState('');
 	const [secret, setSecret] = useAtom(secretAtom);
 	const [open, setOpen] = useState(false);
+	const inputRef = useRef<InputRef>(null);
+	const [form] = Form.useForm();
 
-	function okHandler() {
-		setOpen(false);
-		setSecret(inputValue);
-		setInputValue('');
-	}
+	useEffect(() => {
+		function keyDownHandler(e: KeyboardEvent) {
+			if (e.key === 'Enter') {
+				form.submit();
+			}
+		}
+		window.addEventListener('keydown', keyDownHandler);
+		return () => window.removeEventListener('keydown', keyDownHandler);
+	}, [form]);
 
 	function cancelHandler() {
 		setOpen(false);
-		setInputValue('');
+		form.resetFields();
 	}
 
 	function authButtonClickHandler() {
@@ -26,19 +37,35 @@ export function Authentication() {
 			return;
 		}
 		setOpen(true);
+		setTimeout(() => {
+			// Doesn't work without a slight delay
+			inputRef.current?.focus();
+		}, 100);
+	}
+
+	function formSubmitHandler(formValues: FormValues) {
+		setSecret(formValues.SECRET);
+		form.resetFields();
+		setOpen(false);
+	}
+
+	function okHandler() {
+		form.submit();
 	}
 
 	return (
 		<>
 			<AuthButton onClick={authButtonClickHandler} isLoggedIn={secret !== null} />
-			<Modal
-				title="Enter secret"
-				open={open}
-				onOk={okHandler}
-				onCancel={cancelHandler}
-				okButtonProps={{ disabled: inputValue.length === 0 }}
-			>
-				<Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Enter secret" />
+			<Modal title="Admin Zone" open={open} onOk={okHandler} onCancel={cancelHandler}>
+				<Form form={form} onFinish={formSubmitHandler} layout="horizontal" requiredMark="optional">
+					<Form.Item<FormValues>
+						label="Secret"
+						name="SECRET"
+						rules={[{ required: true, message: 'Please enter a secret!' }]}
+					>
+						<Input ref={inputRef} placeholder="Enter secret" />
+					</Form.Item>
+				</Form>
 			</Modal>
 		</>
 	);
